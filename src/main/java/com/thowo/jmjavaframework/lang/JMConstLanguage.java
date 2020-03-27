f2a33f7b4e142d3fd49860723cbd0bb98e879d8b
jimi <jimmyhardmondow@gmail.com>
2020-03-27 22:23:27 +0800

    New Test

---
+++ src/main/java/com/thowo/jmjavaframework/lang/JMConstLanguage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thowo.jmjavaframework.lang;
+
+/**
+ *
+ * @author jimi
+ */
+public class JMConstLanguage {
+    public static final String LANG_ENG="0001";
+    public static final String LANG_ID="0002";
+}
--- src/main/java/com/thowo/jmjavaframework/lang/JMMessage.java
+++ src/main/java/com/thowo/jmjavaframework/lang/JMMessage.java
@@ -10,5 +10,23 @@ package com.thowo.jmjavaframework.lang;
  * @author jimi
  */
 public class JMMessage {
+    private String langId;
+    private String msgId;
+    private String msg;
     
+    public JMMessage(String langId, String msgId, String msg){
+        this.langId=langId;
+        this.msgId=msgId;
+        this.msg=msg;
+    }
+    
+    public String getLangId(){
+        return this.langId;
+    }
+    public String getMsgId(){
+        return this.msgId;
+    }
+    public String getMsg(){
+        return this.msg;
+    }
 }
